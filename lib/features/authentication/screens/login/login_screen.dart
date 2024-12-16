df2ddd88f1f1df44bd845ac55d4fea4cd0661ede
MansoorKhan68 <mansoorkhanlkl99@gmail.com>
2024-12-16 16:16:08 +0500

    update the Code

--- lib/features/authentication/screens/login/login_screen.dart
+++ lib/features/authentication/screens/login/login_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:khyber_kart/commons/styles/spacing_style.dart';
-import 'package:khyber_kart/features/authentication/screens/login/widgets/form_divider.dart';
+import 'package:khyber_kart/commons/widgets/form_divider.dart';
 import 'package:khyber_kart/features/authentication/screens/login/widgets/login_form.dart';
 import 'package:khyber_kart/features/authentication/screens/login/widgets/login_header.dart';
 import 'package:khyber_kart/features/authentication/screens/login/widgets/social_buttons.dart';
 import 'package:khyber_kart/utils/constants/sizes.dart';
+import 'package:khyber_kart/utils/constants/text_strings.dart';
 import 'package:khyber_kart/utils/helpers/helper_functions.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -25,7 +26,10 @@ class LoginScreen extends StatelessWidget {
               LoginForm(),
 
               /// Divider
-              FormDivider(dark: dark),
+              FormDivider(
+                dark: dark,
+                dividerText: TTexts.orSignInWith,
+              ),
               SizedBox(
                 height: TSizes.spaceBtwSections,
               ),
